dd7148e3ba0ef61a1b9912fe55e59e08b4ca1876
Ian Gibson <iangibsonc1@gmail.com>
2018-09-22 11:55:58 -0600

    created game logic

--- memory_game/js/main.js
+++ memory_game/js/main.js
@@ -3,12 +3,36 @@ Main
 \===========*/
 
 
-var cards = ["Queen", "Queen", "King", "King"];
+var cards = [
+	{
+		rank: "queen",
+		suit: "hearts",
+		cardImage: "images/queen-of-hearts.png"
+	},
+	{
+		rank: "queen",
+		suit: "diamonds",
+		cardImage: "images/queen-of-diamonds.png"
+	},
+	{
+		rank: "king",
+		suit: "hearts",
+		cardImage: "images/king-of-hearts.png"
+	},
+	{
+		rank: "king",
+		suit: "diamonds",
+		cardImage: "images/king-of-diamonds"
+	}
+];
+
 var cardsInPlay = [];
 
 /*===========\
 Functions
 \===========*/
+
+//function checForMatch
 function checkForMatch()
 {
 	if(cardsInPlay[0] === cardsInPlay[1])
@@ -21,14 +45,17 @@ function checkForMatch()
 	}
 }
 
+//function flipCard
 function flipCard(cardId)
 {
-	cardsInPlay.push(cards[cardId]);	
-	console.log("User flipped " + cards[cardId]);
+	cardsInPlay.push(cards[cardId].rank);	
 	if(cardsInPlay.length === 2)
 	{
 		checkForMatch();
 	}
+	console.log("User flipped " + cards[cardId].rank);
+	console.log(cards[cardId].cardImage);
+	console.log(cards[cardId].suit);
 }
 
 flipCard(0);
